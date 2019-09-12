670af9df8ecc03bf446206141f2a943d10273258
losstname <losstname@gmail.com>
2019-09-12 10:48:09 +0700

    Initial commit

---
+++ src/main/groovy/com/losstname/oauth2/Oauth2Application.groovy
@@ -0,0 +1,13 @@
+package com.losstname.oauth2
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Oauth2Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(Oauth2Application, args)
+	}
+
+}
